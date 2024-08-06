a528eabfebaf6fd5a505af347d8ac48e4b363660
Anthony Garcia <Anthony13241@gmail.com>
2024-08-06 02:41:40 +0000

    practice-interview-problem

---
+++ practice-interview-challenge/main.js
@@ -0,0 +1,26 @@
+"use strict";
+function getRangeReport(start, end) {
+    let total = 0;
+    const odds = [];
+    const evens = [];
+    const range = [];
+    for (let i = start; i <= end; i++) {
+        range.push(i);
+        total += i;
+        if (i % 2 === 0) {
+            evens.push(i);
+        }
+        else {
+            odds.push(i);
+        }
+    }
+    const average = (start + end) / 2;
+    const result = {
+        total: total,
+        odds: odds,
+        evens: evens,
+        range: range,
+        average: average
+    };
+    return result;
+}
